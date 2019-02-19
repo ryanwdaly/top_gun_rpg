7ab7fb90059464ddd7f57a2850a8fa11f2e9184f
Ryan Daly <ryan.w.daly90@gmail.com>
2019-02-19 13:52:36 -0500

    finished logic

--- script.js
+++ script.js
@@ -1,95 +1,146 @@
 $(document).ready(function() {
 
-    initialize();
-    selected_character = null;
-    enemy_characters = null;
-    selected_enemy = null; 
+    characters = {
+        maverick: {
+            name: 'maverick',
+            imgFile: 'assets/images/maverick',
+            healthPoints: 100,
+            baseAttack: 50,
+            counterAttack: 5
+
+        },
+        iceman: {
+            name: 'iceman',
+            imgFile: 'assets/images/iceman',
+            healthPoints: 100,
+            baseAttack: 5,
+            counterAttack: 5
+        },
+        goose: {
+            name: 'viper',
+            imgFile: 'assets/images/goose',
+            healthPoints: 100,
+            baseAttack: 5,
+            counterAttack: 5
+        },
+        viper: {
+            name: 'goose',
+            imgFile: 'assets/images/viper',
+            healthPoints: 100,
+            baseAttack: 5,
+            counterAttack: 5
+        }
+    }
     
+    var selectedCharacterName = null;
+    var enemyCharacters = [];
+    var selectedEnemyName = null; 
+    
+    //Generate the character icons in the Character selection container
+    for(var key in characters) {
+        var img = generateImg(characters[key]);
+        img.attr("id", "selection");
+        $("#character-select-container").append(img);
+    }
+       
+   
     //Click to choose a character
     $("body").on("click", "#selection",  function() {
         self = $(this);
-        //Match IMG ID with Character object
-        if (self.attr("id") === "selection")
-        for (i=0; i<character_array.length; i++){
-            if (self.attr("character-name") === character_array[i].name){
-                selected_character = character_array[i];
-                break;
+        
+        //Sort into global variables
+        for (var key in characters) {
+            if (self.attr("character-name") === characters[key].name){
+                selectedCharacterName = characters[key].name;
+            } else {
+                enemyCharacters.push(characters[key].name);
             }
         }
         
+        
         //Add Slected character icon to Your character container
-        var newIcon = generateImg(selected_character)
+        var newIcon = generateImg(idCharacterByName(selectedCharacterName));
         newIcon.attr("id", "player-character")
         $("#player-character-container").append(newIcon);
-
+        
         //add nonselected character icons to enemies container
-        character_array.forEach(function(character) {
-            if (character.name != selected_character.name) {
-                newIcon = generateImg(character);
-                newIcon.attr("class", "enemy-character")
-                $("#enemies-container").append(newIcon);
-            }
+        enemyCharacters.forEach(function(enemy) {
+            newIcon = generateImg(idCharacterByName(enemy));
+            newIcon.attr({
+                "class": "enemy-character",
+                "character-name": enemy
+            });
+            $("#enemies-container").append(newIcon);
         });
+            
+        //Remove character selection icons
+        $("#character-select-container").remove();
+        
     });
-      //Select enemy to attack
-      $("body").on("click", ".enemy-character", function() {
-            var enemy_icon = generateImg();
-            enemy_icon.attr("class", "character-incons")
-            $("#fight-section-container").append(enemy_icon);
-            $("#fight-section-container").append(selected_character);
-      });
-
-
-// SELECET CHARACTER BUTTON FUNCTIONS
-
-
-
-
-// ATTACK BUTTON
-$("#attack-button").on('click', function() {
     
+    //Select enemy to attack
+    $("body").on("click", ".enemy-character", function() {
+        self = $(this);
+
+        selectedEnemyName = self.attr("character-name");
+
+        var enemy_icon = generateImg(idCharacterByName(selectedEnemyName));
+        var player_icon = generateImg(idCharacterByName(selectedCharacterName));
+        enemy_icon.attr({
+            class: "fighter-section-icons",
+            id: "attacking-enemy"
+        });
+        player_icon.attr({
+            class: "fighter-section-icons",
+            id: "attacking-player"
+        });
+
+        $("#fight-section-container").append(player_icon);
+  
+        $("#fight-section-container").append(enemy_icon);
+      
     
+        //Create attack button
+        var attack_button = $("<button>")
+        attack_button.attr({
+                id: 'attack-button',
+                type: "button",
+                class: 'btn',
+                value: "Attack!"
+        });
+                $("#attack-button-container").append(attack_button);
+                $("#attack-button").append("Attack!")
+        //         $("#enemies-container").remove(self);
+    });
+
+    //attack button
+    $("body").on("click", "#attack-button", function() {
+      
+        playerCharacter = idCharacterByName(selectedCharacterName);
+        enemyCharacter = idCharacterByName(selectedEnemyName);
+        
+        enemyCharacter.healthPoints -= playerCharacter.baseAttack;
+        if (enemyCharacter.healthPoints <= 0) {
+            alert("Won")
+        } else {
+            playerCharacter.healthPoints -= enemyCharacter.baseAttack
+        }
+
+        if (playerCharacter.healthPoints <= 0 ) {
+            alert("lost")
+        }
+        
+        
+    });
 });
 
-
-
 // HELPER METHODS
-function initialize() {
-    var maverick = {
-        name: 'maverick',
-        img_file: 'assets/images/maverick',
-        health_points: 100,
-        base_attack: 5,
-        counter_attack: 5
-        
-    }
-    var iceman = {
-        name: 'iceman',
-        img_file: 'assets/images/iceman',
-        health_points: 100,
-        base_attack: 5,
-        counter_attack: 5
-    }
-    var goose = {
-        name: 'viper',
-        img_file: 'assets/images/goose',
-        health_points: 100,
-        base_attack: 5,
-        counter_attack: 5
-    }
-    var viper = {
-        name: 'goose',
-        img_file: 'assets/images/viper',
-        health_points: 100,
-        base_attack: 5,
-        counter_attack: 5
-    }
-    character_array = [maverick, iceman, goose, viper];
-    
-    for (i = 0; i < character_array.length; i++) {
-        newIcon = generateImg(character_array[i])
-        newIcon.attr("id", "selection")
-        $("#character-select-container").append(newIcon);
+
+function idCharacterByName(name) {
+    for (var key in characters) {
+       if(characters[key].name === name) {
+           return characters[key];
+       }
     }
 }
 
@@ -99,7 +150,7 @@ function generateImg(object) {
     newIcon.attr({
         "character-name": object.name,
         class: "character-icons",
-        src: object.img_file + ".jpg",
+        src: object.imgFile + ".jpg",
     });
     return newIcon;
 }
@@ -114,16 +165,14 @@ function addCharacterList() {
         }
     }
 
-    function addComputerCharacters() {
+function addComputerCharacters() {
 
-        for(i=0; i < character_array.length; i++){
-            if(character_array[i] != player_character) {
-                computer_characters.push(character_array[i]);
-            }
+    for(i=0; i < character_array.length; i++){
+        if(character_array[i] != player_character) {
+            computer_characters.push(character_array[i]);
         }
     }
+}
     
 
 
-
-});
